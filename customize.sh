0d18b90ec9b49099678a9d39b7a28c70f383fc01
NTeditor <nteditor4@gmail.com>
2025-07-08 13:39:51 +0300

    Добавленны client_hello и quic_initial для фейков

--- customize.sh
+++ customize.sh
@@ -1,34 +1,34 @@
 # shellcheck disable=SC2148
 set_binary() {
 	ui_print "- Копирование nfqws"
-	cp "$MODPATH/binary/nfqws_$ARCH" "$MODPATH/system/bin/nfqws"
+	cp "$MODPATH/binaries/nfqws_$ARCH" "$MODPATH/system/bin/nfqws"
 }
 
 set_list() {
 	ui_print "- Создание директории для zapret"
 	mkdir -p "/data/adb/zapret"
-	cp "$MODPATH/list/DPI_list.txt" "/data/adb/zapret/DPI_list.txt"
-	cp "$MODPATH/list/DPI_ignore.txt" "/data/adb/zapret/DPI_ignore.txt"
+	cp "$MODPATH/list/zapret-hosts-user.txt" "/data/adb/zapret/zapret-hosts-user.txt"
+	cp "$MODPATH/list/zapret-hosts-user-exclude.txt" "/data/adb/zapret/zapret-hosts-user-exclude.txt"
 }
 
 set_permission() {
 	ui_print "- Установка разрешений"
 	set_perm "$MODPATH/system/bin/zapret" 0 0 744
 	set_perm "$MODPATH/system/bin/nfqws" 0 0 744
-	set_perm "/data/adb/zapret/DPI_list.txt" 0 0 666
-	set_perm "/data/adb/zapret/DPI_ignore.txt" 0 0 666
+	set_perm "/data/adb/zapret/zapret-hosts-user.txt" 0 0 666
+	set_perm "/data/adb/zapret/zapret-hosts-user-exclude.txt" 0 0 666
 }
 
 cleanup() {
-	ui_print "- Очистка"	
-	rm -rf "$MODPATH/binary"
+	ui_print "- Очистка"
+	rm -rf "$MODPATH/binaries"
 	rm -rf "$MODPATH/list"
 }
 
-if [[ "$ARCH" != "arm" && \
-  	  "$ARCH" != "arm64" && \
-	  "$ARCH" != "x86" && \
-	  "$ARCH" != "x64" ]]; then
+if [[ "$ARCH" != "arm" &&
+	"$ARCH" != "arm64" &&
+	"$ARCH" != "x86" &&
+	"$ARCH" != "x64" ]]; then
 	abort "- Архитектура $ARCH не поддерживается!"
 else
 	set_binary
--- service.sh
+++ service.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
 
-if [ -f /data/adb/modules/zapret/autostart ]; then
+if false; then
     su -c "zapret start"
 fi
