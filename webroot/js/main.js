31423935a9444270ea08b8afb5267b90cdc1d5ec
NTeditor <nteditor4@gmail.com>
2025-07-11 15:43:46 +0300

    Базовая структура

---
+++ webroot/js/main.js
@@ -0,0 +1,38 @@
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const enableButton = document.getElementById('enableButton');
+const disableButton = document.getElementById('disableButton');
+const restartButton = document.getElementById('restartButton');
+const statusButton = document.getElementById('statusButton');
+
+function handleStart() {
+  console.log('Кнопка "start" нажата!');
+}
+
+function handleStop() {
+  console.log('Кнопка "stop" нажата!');
+}
+
+function handleEnable() {
+  console.log('Кнопка "enable" нажата!');
+}
+
+function handleDisable() {
+  console.log('Кнопка "disable" нажата!');
+}
+
+function handleRestart() {
+  console.log('Кнопка "restart" нажата!');
+}
+
+async function handleStatus() {
+  console.log('Кнопка "status" нажата!');
+}
+
+// Прикрепляем обработчики событий 'click' к каждой кнопке
+startButton.addEventListener('click', handleStart);
+stopButton.addEventListener('click', handleStop);
+enableButton.addEventListener('click', handleEnable);
+disableButton.addEventListener('click', handleDisable);
+restartButton.addEventListener('click', handleRestart);
+statusButton.addEventListener('click', handleStatus);
