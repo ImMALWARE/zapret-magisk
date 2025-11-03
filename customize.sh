1392247feb28ccacf598611a660885b67164695b
ImMALWARE <contact@malw.link>
2025-11-03 20:55:08 +0300

    Новые стратегии, resolves #24, resolves #19

--- customize.sh
+++ customize.sh
@@ -1,6 +1,5 @@
 #!/system/bin/sh
 
-
 ui_print "Копирование nfqws для $ARCH"
 case "$ARCH" in
     arm64)   cp -af "$MODPATH/common/nfqws_arm64" "$MODPATH/system/bin/nfqws";;
@@ -22,13 +21,18 @@ cat > "/data/adb/zapret/autohosts.txt" << EOL
 7tv.app
 amnezia.org
 ampproject.org
+api.cloudflareclient.com
+apkmirror.com
 appspot.com
 avira.com
+blancvpn.net
 botnadzor.org
 cdninstagram.com
 censorship.no
 censortracker.org
+chess.com
 cloudflare-ech.com
+cock.li
 conversations.im
 discord-attachments-uploads-prd.storage.googleapis.com
 discord.com
@@ -36,60 +40,87 @@ discord.gg
 discord.media
 discordapp.com
 discordapp.net
+dolphin-anty.com
+emkei.cz
 engage.cloudflareclient.com
 facebook.com
 fbcdn.net
 fbsbx.com
 ficbook.net
 files.catbox.moe
+forumlib.me
+freedomhouse.org
 gekkk.co
 ggpht.com
 godaddy.com
+google.ua
 googlevideo.com
 imagedelivery.net
 instagram.com
+ipvanish.com
+itch.io
 jnn-pa.googleapis.com
 jut.su
+kemono.su
 linkedin.com
 linktr.ee
 lolz.guru
+matrix-client.matrix.org
 matrix.org
 matrix.to
 medium.com
+mega.co.nz
+mega.nz
+mullvad.net
 musixmatch.com
 news.google.com
 nnmclub.to
+nordvpn.com
 notepad-plus-plus.org
 ntc.party
 pages.dev
 patreon.com
 picuki.com
 play.google.com
+privateinternetaccess.com
 prnt.sc
 proton.me
+protonmail.ch
 protonmail.com
 protonvpn.com
+protonvpn.net
 psiphon.ca
 quora.com
+redshieldvpn.com
 rentry.co
 rentry.org
 riseup.net
 roskomsvoboda.org
+rutracker.cc
 rutracker.org
+sb.scorecardresearch.com
 signal.org
+snapchat.com
+sndcdn.com
 soundcloud.com
+speedtest.net
 t.co
 te-st.org
+thonny.org
+tmailor.com
 torproject.org
+tuta.com
 twimg.com
 twitter.com
 ulta.team
 vector.im
 viber.com
+vpnlove.me
 wide-youtube.l.google.com
 windscribe.com
 wixmp.com
 x.com
+xvpn.io
 youtu.be
 youtube-nocookie.com
 youtube-ui.l.google.com
@@ -98,6 +129,7 @@ youtubeembeddedplayer.googleapis.com
 youtubei.googleapis.com
 yt-video-upload.l.google.com
 yt.be
+yt3.googleusercontent.com
 ytimg.com
 ytimg.l.google.com
 zelenka.guru
@@ -142,13 +174,14 @@ EOL
 chmod 666 "/data/adb/zapret/ignore.txt";
 
 cat > "/data/adb/zapret/config.txt" << EOL
---filter-udp=443 --hostlist={hosts} --hostlist-exclude={ignore} --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic={quicgoogle} --new 
---filter-udp=50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new 
---filter-tcp=80 --hostlist-auto={hosts} --hostlist-exclude={ignore} --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new 
---filter-tcp=443 --hostlist-auto={hosts} --hostlist-exclude={ignore} --dpi-desync=split2 --dpi-desync-split-seqovl=652 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern={tlsgoogle}
+--filter-udp=443 --hostlist-auto={hosts} --hostlist-exclude={ignore} --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic={quicgoogle} --dpi-desync-fwmark=0x40000000/0x40000000 --new
+--filter-udp=19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fwmark=0x40000000/0x40000000 --new
+--filter-tcp=2053,2083,2087,2096,8443 --hostlist-domains=discord.media --dpi-desync=multisplit --dpi-desync-split-seqovl=568 --dpi-desync-split-pos=1 --dpi-desync-split-seqovl-pattern={tlsgoogle} --dpi-desync-fwmark=0x40000000/0x40000000 --new
+--filter-tcp=443 --hostlist-auto={hosts} --hostlist-exclude={ignore} --ip-id=zero --dpi-desync=multisplit --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=1 --dpi-desync-split-seqovl-pattern={tlsgoogle} --dpi-desync-fwmark=0x40000000/0x40000000 --new
+--filter-tcp=80 --hostlist={hosts} --hostlist-exclude={ignore} --dpi-desync=multisplit --dpi-desync-split-seqovl=568 --dpi-desync-split-pos=1 --dpi-desync-split-seqovl-pattern={tlsgoogle} --dpi-desync-fwmark=0x40000000/0x40000000
 EOL
 chmod 666 "/data/adb/zapret/config.txt";
 
 touch "/data/adb/zapret/autostart"
 
-ui_print "Прочитайте гайд на https://wiki.malw.link/w/zapret"
+ui_print "Прочитайте гайд на https://wiki.malw.link/network/vpns/zapret"
--- system/bin/zapret_check.sh
+++ system/bin/zapret_check.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if ! command -v curl >/dev/null 2>&1; then
-    echo "curl not installed!"
+if command -v curl >/dev/null 2>&1; then
+    CURL_CMD="curl"
+elif [ -x "/data/data/com.termux/files/usr/bin/curl" ]; then
+    CURL_CMD="/data/data/com.termux/files/usr/bin/curl"
+else
+    echo "curl is not installed! Install Termux app and run 'pkg install curl' command."
     read -p ""
     exit 1
 fi
@@ -9,7 +13,7 @@ fi
 while IFS= read -r line; do
     if [ -n "$line" ]; then
         printf "Checking %s... " "$line"
-        response=$(curl --connect-timeout 5 --max-time 5 -s --show-error -o /dev/null -w "%{http_code}" "https://$line" 2>&1)
+        response=$($CURL_CMD --connect-timeout 5 --max-time 5 -s --show-error -o /dev/null -w "%{http_code}" "https://$line" 2>&1)
         if [[ "$response" == *000 ]]; then
             echo "${response%000}"
         else
